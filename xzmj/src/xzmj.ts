7a80c3860af57b94f581439b1422b5dd4c3ec977
John39 <player039@outlook.com>
2021-03-03 10:39:08 +0800

    xzmj index

---
+++ xzmj/src/xzmj.ts
@@ -0,0 +1,10 @@
+module GMD_xzmj{
+	export const enum GameUITag{
+		openRoomViewUI = 1001,
+		openRoomUI = 1002,
+		openStart = 1003,
+		openBalance = 1004
+	}
+	export let scene:game.IScene;
+	export let isActive:boolean;
+	
